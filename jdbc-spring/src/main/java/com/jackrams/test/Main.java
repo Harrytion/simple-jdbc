397cba5606d661b2609c57722be27fc5e976feda
Jackrams <764019983@qq.com>
2018-05-05 03:46:17 +0800

    fix Example Bug

--- jdbc-spring/src/main/java/com/jackrams/test/Main.java
+++ jdbc-spring/src/main/java/com/jackrams/test/Main.java
@@ -1,5 +1,8 @@
 package com.jackrams.test;
 
+import com.jackrams.domain.Example;
+import com.jackrams.domain.SelectExample;
+import com.jackrams.helpper.PropertyHelpper;
 import com.jackrams.test.config.Configurations;
 import com.jackrams.test.dao.PersonDao;
 import com.jackrams.test.dao.UserDao;
@@ -10,6 +13,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.File;
+import java.util.List;
 import java.util.UUID;
 
 public class Main {
@@ -21,19 +25,33 @@ public class Main {
         Person person =new Person();
         person.setAge(18);
         person.setDes("P--哈哈，我是注释");
-        person.setImg(FileUtils.readFileToByteArray(new File("/home/hadoop/imgs.png")));
+        person.setImg(FileUtils.readFileToByteArray(new File("D:\\data\\img\\photo1_1.jpg")));
         person.setuId(UUID.randomUUID().toString().replaceAll("-",""));
         person.setUserName("__P我是姓名");
         User user =new User();
 
         user.setAge(18);
         user.setDes("UUU--哈哈，我是注释");
-        user.setImg(FileUtils.readFileToByteArray(new File("/home/hadoop/imgs.png")));
+        user.setImg(FileUtils.readFileToByteArray(new File("D:\\data\\img\\photo1_10.jpg")));
         user.setuId(UUID.randomUUID().toString().replaceAll("-",""));
         user.setUserName("_UUU我是姓名UUU");
 
         userDao.insert(user);
 
         personDao.insert(person);
+
+
+        SelectExample example = SelectExample.custom().addPage(1).addPageSize(5);
+         example =((SelectExample) example.andLike(PropertyHelpper.getPropertyByField(User.class, "des"), "UUU%").end()).addPage(1).addPageSize(5)
+                 .addAsc(PropertyHelpper.getPropertyByField(User.class,"uId"));
+        List<User> users = userDao.selectListByExample(example);
+        for(User usert : users){
+            String s = usert.getuId();
+            System.out.println(s);
+
+        }
+
+
     }
+
 }
